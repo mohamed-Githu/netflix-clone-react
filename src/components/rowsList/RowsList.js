9a9cdd3442c0b697f15ca74f08c4f21bff21bc9c
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2021-09-24 20:28:45 +0200

    adding env variables

--- src/components/rowsList/RowsList.js
+++ src/components/rowsList/RowsList.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import MoviesRow from '../moviesRow/MoviesRow';
+import MoviesRow from "../moviesRow/MoviesRow";
 
-import requests from '../../requests';
+import requests from "../../requests";
 
 const RowsList = () => {
   return (
     <div>
-      {
-        requests.map((request, index) => {
-          return (
-            <MoviesRow 
-              key={index}
-              isLarge={request.title === 'NETFLIX ORIGINALS' ? true : false} 
-              title={request.title} 
-              fetchUrl={request.fetchMovies}
-            />
-          )
-        })
-      }
+      {requests.map((request, index) => {
+        return (
+          <MoviesRow
+            key={index}
+            isLarge={request.title === "NETFLIX ORIGINALS" ? true : false}
+            title={request.title}
+            fetchUrl={request.fetchMovies}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default RowsList
+export default RowsList;
